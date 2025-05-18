5cd5e3fe516573347d264c077b7c96e1848f0875
Rafflesian <56321479+Rafflesian@users.noreply.github.com>
2025-05-18 13:35:23 -0400

    Create unpacker.py

---
+++ unpacker.py
@@ -0,0 +1,58 @@
+import subprocess;
+from pathlib import Path;
+
+def fnParseDirectory(szPrompt: str, szSuffix: str = ""):
+    
+    szSuffix = szSuffix.replace(" ", "").lower();
+
+    if(szSuffix):
+        szPrompt += f" ({szSuffix})";
+
+    szPrompt += ": ";
+
+    while True:
+
+        szReturn = input(szPrompt).replace("\"", "");
+        pPath = Path(szReturn);
+
+        if(not pPath.exists()):
+            print(f"Invalid directory '{szReturn}'");
+            continue;
+        
+        if(szSuffix):
+
+            if(not pPath.is_file()):
+                print(f"Provided directory is not a file");
+                continue;
+        
+            if(pPath.suffix.lower() != szSuffix):
+                print(f"Provided file doesn't match suffix '{szSuffix}'");
+                continue;
+
+        break;
+
+    return szReturn;
+
+szUnpacker  = fnParseDirectory("Unpacker Route", '.exe');
+szPack      = fnParseDirectory(".pak Route");
+szOutput    = fnParseDirectory("Output Route");
+
+iFileCount = 0;
+
+while True:
+
+    szFile = f"common_{'{:0>5}'.format(iFileCount)}.pak";
+    szFilePath = f"{szPack}/{szFile}";
+
+    if(not Path(szFilePath).exists()):
+        break;
+
+    print(f"Processing: {szFilePath}");
+    subprocess.run([szUnpacker, szFilePath], cwd=szOutput, check = True);
+
+    iFileCount += 1;
+
+if(iFileCount):
+    print(f"{iFileCount} files processed");
+else:
+    print("No .pak files were found");
